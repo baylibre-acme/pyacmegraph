dbf3b665cb63f9088feb99906fc4b8b655e686c5
Sebastien Jan <sjan@baylibre.com>
2017-04-06 09:57:52 +0200

    fix energy computation naming (to Accumulated Energy)
    The Energy computation was erroneuously named 'Mean Energy'.
    Fix UI to reflect what it really is: Accumulated energy
    (over the zoom window samples).
    
    Signed-off-by: Sebastien Jan <sjan@baylibre.com>

--- pyacmegraph.py
+++ pyacmegraph.py
@@ -628,7 +628,7 @@ zoomp_mean_tmpl = {'name': 'Float', 'type': 'float', 'value': 0, 'step': 0.001, 
 zoomp = {'name': 'Zoom plot', 'type': 'group', 'children': [
         {'name': 'width (ms)', 'type': 'float', 'value': 0, 'readonly': True},
         {'name': 'Mean ' + dispstr['pwr_ishunt_str'], 'type': 'group', 'children': []},
-        {'name': 'Mean Energy (J or Ws)', 'type': 'group', 'children': []},
+        {'name': 'Accumulated Energy (J or Ws)', 'type': 'group', 'children': []},
     ]}
 for t in databufs:
     m = copy.deepcopy(zoomp_mean_tmpl)
@@ -1004,14 +1004,14 @@ def update_zoomp():
         # print "gdata : ", gdata.shape
         mean = gdata[gdata[:,0].searchsorted(minX):gdata[:,0].searchsorted(maxX), 1].mean(0)
         pt.child('Zoom plot', 'Mean ' + dispstr['pwr_ishunt_str'], 'm:' + t['deviceid']).setValue(mean)
-        # compute energy
+        # compute accumulated energy
         tmpdata = gdata[gdata[:,0].searchsorted(minX):gdata[:,0].searchsorted(maxX), :]
         energy = 0
         for i,d in enumerate(tmpdata):
             if i !=0:
                 energy += (tmpdata[i,0] - tmpdata[i-1,0]) * tmpdata[i, 1]
         energy = energy / 1000000  # account for ms and mW
-        pt.child('Zoom plot', 'Mean Energy (J or Ws)', 'e:' + t['deviceid']).setValue(energy)
+        pt.child('Zoom plot', 'Accumulated Energy (J or Ws)', 'e:' + t['deviceid']).setValue(energy)
         # print("  Energy:%f" %(energy))
 
 # Update vbat mean computation area fields
